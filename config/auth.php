512a91de96967f4aea3a27262bd1737fb02b2625
AliqNurShiddiq-TelU <aliqnurshiddiq@student.telkomuniversity.ac.id>
2025-11-30 16:23:02 +0700

    login/register

--- config/auth.php
+++ config/auth.php
@@ -42,6 +42,13 @@ return [
         ],
     ],
 
+    'providers' => [
+    'users' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\User::class,
+    ],
+],
+
     /*
     |--------------------------------------------------------------------------
     | User Providers
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', [AuthController::class, 'dashboard'])
+    ->middleware('auth')
+    ->name('dashboard');
